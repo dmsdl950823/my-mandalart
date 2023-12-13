370a760dbd7790016b45fd167ef26c7750fe316b
정재은 <dmsdl950823@gmail.com>
2023-12-13 17:56:40 +0900

    Revert "Added # 만다라트 차트 만들었어용"
    This reverts commit 18f2e81399450c48931d27359d3b9c2c04ac9ea1.

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,113 @@
-'use client'
-import React, { useState } from 'react'
-
-import { Dosis } from 'next/font/google'
-import './style/common.css'
-
-const dosis = Dosis({ subsets: ['latin'] })
+import Image from 'next/image'
 
 export default function Home() {
-  const centerTarget = [
-    { field: 0, value: '', placeholder: '목표1' },
-    { field: 1, value: '', placeholder: '목표2' },
-    { field: 2, value: '', placeholder: '목표3' },
-    { field: 3, value: '', placeholder: '목표4' },
-    { field: 4, value: '', placeholder: '최종 목표', disabled: true, center: true },
-    { field: 5, value: '', placeholder: '목표5' },
-    { field: 6, value: '', placeholder: '목표6' },
-    { field: 7, value: '', placeholder: '목표7' },
-    { field: 8, value: '', placeholder: '목표8' },
-  ]
-  const aroundTarget = [
-    { field: 0, value: '', placeholder: '' },
-    { field: 1, value: '', placeholder: '' },
-    { field: 2, value: '', placeholder: '' },
-    { field: 3, value: '', placeholder: '' },
-    { field: 4, value: '', placeholder: '목표', disabled: true, center: true },
-    { field: 5, value: '', placeholder: '' },
-    { field: 6, value: '', placeholder: '' },
-    { field: 7, value: '', placeholder: '' },
-    { field: 8, value: '', placeholder: '' },
-  ]
-
-
-  const [editArea, setEditArea] = useState(undefined) // 편집 영역
-  const [editing, setEditing] = useState(undefined) // 편집 칸
-
-  const onClick = e => {
-    if (e.target.parentElement.dataset.code) {
-      const codes = e.target.parentElement.dataset.code.split('_')
-      const area = codes[1]
-      const code = codes[2]
-
-      setEditArea(Number(area))
-      setEditing(Number(code))
-    }
-  }
-
-  /**
-   * 빠져나갈 경우 초기화
-   */
-  const onBlur = e => {
-    setEditArea(undefined)
-    setEditing(undefined)
-  }
-
-  /**
-   * 텍스트 정보 변경시 사용
-   */
-  const onChange = (e, value) => {
-    const text = e.target.value
-    aroundTarget[editing].value = text
-  }
-
-  const inputs = (targets, area) => targets.map(({ field, value, placeholder, center }, key) => {
-    const className = `text-box ${center ? '-center' : ''}`
-
-    return (
-      <div
-        key={key}
-        className={className}
-        data-code={`box_${area}_${field}`}
-        onClick={onClick}
-        onBlur={onBlur}
-        >
-          {
-            // 편집중 && 필드가 동일한 경우만 textarea 띄워줌
-            editArea !== undefined && editArea === area &&
-            editing !== undefined && editing === field
-            ?
-            <textarea onChange={e => onChange(e, value)} /> :
-            <span className={value ? '-value' : '-placeholder'}>{ value || placeholder }</span>
-          }
-      </div>
-    )
-  })
-
-
-
   return (
-    <div className="wrapper">
-      <section className="flex min-h-screen flex-col items-center justify-center space-between my-auto m-auto w-[900px]">
-        <h1 className={dosis.className}>2024 My Mandalart</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.js</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
 
-        <main className="grid grid-cols-3 gap-5">
-          <div className="box-area grid grid-cols-3"> {inputs(aroundTarget, 0)} </div>
-          <div className="box-area grid grid-cols-3"> {inputs(aroundTarget, 1)} </div>
-          <div className="box-area grid grid-cols-3"> {inputs(aroundTarget, 2)} </div>
-          <div className="box-area grid grid-cols-3"> {inputs(aroundTarget, 3)} </div>
-          <div className="box-area grid grid-cols-3"> {inputs(centerTarget, 4)} </div>
-          <div className="box-area grid grid-cols-3"> {inputs(aroundTarget, 5)} </div>
-          <div className="box-area grid grid-cols-3"> {inputs(aroundTarget, 6)} </div>
-          <div className="box-area grid grid-cols-3"> {inputs(aroundTarget, 7)} </div>
-          <div className="box-area grid grid-cols-3"> {inputs(aroundTarget, 8)} </div>
-        </main>
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
 
-        <article>
-          <textarea className="box-area w-100"></textarea>
-        </article>
-      </section>
-    </div>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore starter templates for Next.js.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
   )
 }
